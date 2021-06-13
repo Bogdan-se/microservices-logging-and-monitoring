802bfe685b165e5eb11bec44b9fd26cae9c62781
Bohdan S <semenovych.b@gmail.com>
2021-06-13 18:21:40 +0300

    System-observability-session-3

--- authors/src/app.module.ts
+++ authors/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 import { AuthorsModule } from './modules/authors/authors.module';
 import { HttpModule } from './modules/http/http.module';
@@ -8,14 +9,23 @@ import { JaegerMiddleware } from './modules/jaeger/jaeger.middleware';
 import { LoggerModule } from './modules/logger/logger.module';
 import { LoggerMiddleware } from './modules/logger/logger.middleware';
 import { RedisModule } from './modules/redis/redis.module';
+import { PrometheusModule } from './modules/prometheus/prometheus.module';
+import { PrometheusInterceptor } from './modules/prometheus/prometheus.interceptor';
 
 @Module({
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: PrometheusInterceptor,
+    },
+  ],
   imports: [
     RedisModule,
     AuthorsModule,
     LoggerModule.forRoot(),
     JaegerModule,
     HttpModule,
+    PrometheusModule,
   ],
 })
 export class AppModule implements NestModule {
